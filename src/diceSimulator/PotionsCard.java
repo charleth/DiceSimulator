e94681927d6d08e5830a676c47459d36ea6d4de7
charleth <C.Gruben@gmx.de>
2021-03-04 19:22:16 +0100

    PotionsCard finished, WildMagicCard added
    finished with the PotionsCard for now, added a new card as a stub (for
    now)

--- src/diceSimulator/PotionsCard.java
+++ src/diceSimulator/PotionsCard.java
@@ -17,17 +17,24 @@ public class PotionsCard extends JPanel implements ActionListener {
 	JPanel lowerPanelPotions = new JPanel();
 
 	JButton normalHealing = new JButton("potion of healing");
-	JButton greaterHealing = new JButton("potion of \n greater healing");
-	JButton superiorHealing = new JButton("potion of \n superior healing");
-	JButton supremeHealing = new JButton("potion of \n supreme healing");
+	JButton greaterHealing = new JButton("potion of greater healing");
+	JButton superiorHealing = new JButton("potion of superior healing");
+	JButton supremeHealing = new JButton("potion of supreme healing");
 
 	JLabel potionselect = new JLabel("<select a potion>");
-	JLabel potionResult = new JLabel(" ");
+	JLabel potionResultLabel = new JLabel(" ");
 	JLabel gulp = new JLabel(" ");
 
 	int amountPotionDice = 0;
+	JLabel potionselectLabel = new JLabel("<select a potion>");
+	String potionselectString = (" ");
 	int sidesPotionDice = 0;
 	int modifierPotionDice = 0;
+	int potionresult = 0;
+	int modresult = 0;
+	int result = 0;
+	JLabel drinkpotionlabel = new JLabel(" ");
+	Random r = new Random();
 
 	public PotionsCard() {
 
@@ -36,88 +43,103 @@ public class PotionsCard extends JPanel implements ActionListener {
 		normalHealing.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				potionselect.setText("You drink a potion of healing");
+
+				potionselect.setText(" ");
+				potionselectString = (" ");
+				drinkpotionlabel.setText("You drink a potion of" + potionselectString + "healing.");
 				gulp.setText("*gulp*");
 				amountPotionDice = 2;
 				sidesPotionDice = 4;
 				modifierPotionDice = 2;
+				modresult = 0;
 				for (int i = 1; i <= amountPotionDice; i++) {
-					Random r = new Random();
 					int low = 1;
 					int high = sidesPotionDice + 1;
-					int result = r.nextInt(high - low) + low + modifierPotionDice;
-					potionResult.setText("You heal for " + String.valueOf(result) + "HP");
+					int result = r.nextInt(high - low) + low;
+					modresult += result;
 				}
+				potionResultLabel.setText("You heal for " + String.valueOf(modresult + modifierPotionDice) + "HP");
 			}
 		});
 		upperPanelPotions.add(greaterHealing);
 		greaterHealing.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				potionselect.setText("You drink a potion of greater healing");
+
+				potionselect.setText(" ");
+				potionselectString = (" greater ");
+				drinkpotionlabel.setText("You drink a potion of" + potionselectString + "healing.");
 				gulp.setText("*gulp*");
 				amountPotionDice = 4;
 				sidesPotionDice = 4;
 				modifierPotionDice = 4;
+				modresult = 0;
 				for (int i = 1; i <= amountPotionDice; i++) {
-					Random r = new Random();
 					int low = 1;
 					int high = sidesPotionDice + 1;
-					int result = r.nextInt(high - low) + low + modifierPotionDice;
-					potionResult.setText("You heal for " + String.valueOf((result) + "HP");
+					int result = r.nextInt(high - low) + low;
+					modresult += result;
 				}
+				potionResultLabel.setText("You heal for " + String.valueOf(modresult + modifierPotionDice) + "HP");
 			}
 		});
 		upperPanelPotions.add(superiorHealing);
 		superiorHealing.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				potionselect.setText("You drink a potion of superior healing");
-				gulp.setText("*gulp*");
+
+				potionselect.setText(" ");
+				potionselectString = (" superior ");
+				drinkpotionlabel.setText("You drink a potion of" + potionselectString + "healing.");
+				gulp.setText("*gulpgulp*");
 				amountPotionDice = 8;
 				sidesPotionDice = 4;
 				modifierPotionDice = 8;
+				modresult = 0;
 				for (int i = 1; i <= amountPotionDice; i++) {
-					Random r = new Random();
 					int low = 1;
 					int high = sidesPotionDice + 1;
-					int result = r.nextInt(high - low) + low + modifierPotionDice;
-					potionResult.setText("You heal for " + String.valueOf(result) + "HP");
+					int result = r.nextInt(high - low) + low;
+					modresult += result;
 				}
+				potionResultLabel.setText("You heal for " + String.valueOf(modresult + modifierPotionDice) + " HP");
 			}
 		});
 		upperPanelPotions.add(supremeHealing);
 		supremeHealing.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				potionselect.setText("You drink a potion of supreme healing");
-				gulp.setText("*gulp*");
-				int amountPotionDice = 10;
-				int sidesPotionDice = 4;
-				int modifierPotionDice = 20;
+
+				potionselect.setText(" ");
+				potionselectString = (" supreme ");
+				drinkpotionlabel.setText("You drink a potion of" + potionselectString + "healing.");
+				gulp.setText("*gulpgulpgulp*");
+				amountPotionDice = 10;
+				sidesPotionDice = 4;
+				modifierPotionDice = 20;
+				modresult = 0;
 				for (int i = 1; i <= amountPotionDice; i++) {
-					Random r = new Random();
 					int low = 1;
 					int high = sidesPotionDice + 1;
 					int result = r.nextInt(high - low) + low;
-					int modresult = result + modifierPotionDice;
-					potionResult.setText(potionResult.getText() + " " + String.valueOf(modresult) + "HP");
-					// TODO this does something, but not what I want it to do. Why? Do I need to do
-					// stuff differently?
-					// different class for rolling the dice?
+					modresult += result;
+//
+//					// TODO
+//					// different class for rolling the dice?
 				}
+				potionResultLabel.setText("You heal for " + String.valueOf(modresult + modifierPotionDice) + " HP");
 
 			}
 		});
 
 		middlePanelPotions.setLayout(new FlowLayout(FlowLayout.LEFT));
-		middlePanelPotions.add(potionselect);
+		middlePanelPotions.add(drinkpotionlabel);
 
 		middlePanelPotions2.setLayout(new FlowLayout(FlowLayout.LEFT));
 		middlePanelPotions2.add(gulp);
 
 		lowerPanelPotions.setLayout(new FlowLayout(FlowLayout.LEFT));
-		lowerPanelPotions.add(potionResult);
+		lowerPanelPotions.add(potionResultLabel);
 
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.add(upperPanelPotions);
@@ -128,8 +150,7 @@ public class PotionsCard extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		potionResult.setText(" ");
-		// TODO this doesnt seem to do anything?! why not?
+		// this doesn't do anything, and it doesn't have to.
 
 	};
 
